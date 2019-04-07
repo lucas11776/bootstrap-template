3fd1ea6c96332ff5c636f0a3a588414a7dcd6abc
Themba <thembangubeni04@gmail.com>
2019-04-07 14:50:13 -0700

    Uploaded template file.

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+﻿(function(){
+    
+    var navbar = $('#sidenav');
+    
+    // Hubuger menu click
+    $('.navbar-btn').click(function(){
+        toggleNavbar();
+    });
+    
+    // close btn click
+    $('.closebtn').click(function(){
+        toggleNavbar();
+    });
+    
+    // open close side navbar
+    function toggleNavbar(){
+        if(navbar.width() == 0) return navbar.width('250px');
+        return navbar.width('0px');
+    }
+    
+}());
